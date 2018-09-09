1e6d94a02fd9cf88b8f30981a860455a14c9a887
Will Speak <lithiumflame@gmail.com>
2018-09-09 14:14:30 +0100

    Switch to NMAKE

--- build.bat
+++ build.bat
@@ -4,14 +4,14 @@ echo "%VCTOOLPATH%
 call "%VCTOOLPATH%\vcvars64.bat"
 mkdir %~dp0\build\win-x64
 pushd %~dp0\build\win-x64
-cmake -D BUILD_SHARED_LIBS=ON ../../webview/
-msbuild
+cmake -G "NMake Makefiles" -D BUILD_SHARED_LIBS=ON ../../webview/
+nmake
 
 call "%VCTOOLPATH%\vcvars32.bat"
 mkdir %~dp0\build\win-x86
 pushd %~dp0\build\win-x86
-cmake -D BUILD_SHARED_LIBS=ON ../../webview/
-msbuild
+cmake -G "NMake Makefiles" -D BUILD_SHARED_LIBS=ON ../../webview/
+nmake
 
 pushd %~dp0
 dotnet build -c Release Webview.Batteries.Win.csproj
